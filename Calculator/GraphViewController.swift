7430d11540e2c75e7275968e035aa45bfe2ea37f
Pascal Attwenger <pascal.attwenger@gmail.com>
2015-07-27 16:36:01 +0200

    New layout: SplitViewController

---
+++ Calculator/GraphViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GraphViewController.swift
+//  Calculator
+//
+//  Created by Pascal Attwenger on 27/07/15.
+//  Copyright © 2015 Pascal Attwenger. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+}
---
+++ Calculator/SplitViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SplitViewController.swift
+//  Calculator
+//
+//  Created by Pascal Attwenger on 27/07/15.
+//  Copyright © 2015 Pascal Attwenger. All rights reserved.
+//
+
+import UIKit
+
+class SplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+    }
+    
+//    override func viewWillAppear(animated: Bool) {
+//        super.viewWillAppear(animated)
+//        self.preferredDisplayMode = .PrimaryOverlay
+//    }
+    
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController, ontoPrimaryViewController primaryViewController: UIViewController) -> Bool {
+        return true
+    }
+
+}
